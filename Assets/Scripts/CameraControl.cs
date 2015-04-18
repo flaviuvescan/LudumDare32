78921a34ff3d37e6bd562ff91fe6d0f24256e76c
Flaviu Vescan <flaviuvescan@gmail.com>
2015-04-18 14:00:44 +0300

    asd
    asd

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+    public float cameraOffset = 2f;
+
+    void Awake()
+    {
+        transform.position = new Vector3(0, 0, -300);
+    }
+
+    void Update()
+    {
+        Vector2 cameraPos = new Vector2(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2);
+
+        Vector3 newCameraPosition = new Vector3(cameraPos.x / cameraOffset, cameraPos.y / cameraOffset, -300);
+
+        transform.localPosition = newCameraPosition;
+    }   
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,21 +4,29 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
     public float playerSpeed = 30f;
-    public Rigidbody body; 
+    public float jumpForce = 1f;
+    public Rigidbody body;
     private float direction = 0f;
+    private float jump = 0f;
     private Vector3 movementVector;
 
     void Update()
     {
         direction = Input.GetAxis("Horizontal");
-
+        
         if (Mathf.Abs(direction) < 0.01f)
         {
             direction = 0f;
         }
 
-        movementVector = new Vector3(direction * playerSpeed * Time.deltaTime, 0,0);
+        if(Input.GetButtonDown("Jump"))
+        {
+            body.AddForce(Vector3.up * jumpForce * 1000f * Time.deltaTime);
+        }
+
+        movementVector = new Vector3(direction * playerSpeed  * Time.deltaTime, 0, 0);
 
         transform.Translate(movementVector);
     }
+
 }
