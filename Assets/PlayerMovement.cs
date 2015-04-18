6ba10e93c97a89d4ba38cca9e7dac82672b746b2
Flaviu Vescan <flaviuvescan@gmail.com>
2015-04-18 13:00:39 +0300

    asd
    asd

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float playerSpeed = 30f;
+    public Rigidbody body; 
+    private float direction = 0f;
+    private Vector3 movementVector;
+
+    void Update()
+    {
+        direction = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(direction) < 0.01f)
+        {
+            direction = 0f;
+        }
+
+        movementVector = new Vector3(direction * playerSpeed * Time.deltaTime, 0,0);
+
+        transform.Translate(movementVector);
+    }
 }
